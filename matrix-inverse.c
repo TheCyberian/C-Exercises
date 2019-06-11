38bf30d6171e1cf49adf360b52ba4e2d3ccd5b35
Utkarsh Raghav <43128146+mrNobody1618@users.noreply.github.com>
2019-06-11 16:19:13 +0530

    Create matrix-inverse.c

---
+++ matrix-inverse.c
@@ -0,0 +1,27 @@
+/* Read input from STDIN. Print your output to STDOUT*/
+#include<stdio.h>
+int main(int argc, char *a[])
+{
+	int i=0, j=0,n,m;
+	scanf("%d %d", &n, &m);
+	int arr1[n][m];
+	for(i=0;i<n;i++){
+		for(j=0;j<m;j++){
+		scanf("%d", &arr1[i][j]);
+		}
+	}
+
+	for(i=0;i<n;i++){	
+		for(j=0;j<m;j++){
+			if(j==m-1){
+				printf("%d", arr1[j][i]);
+			}
+			else{
+				printf("%d ", arr1[j][i]);
+			}
+		}
+		if(i<m-1){
+       printf("\n");
+     }
+	}
+}
